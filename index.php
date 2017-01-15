9bf99f53363a09d7f8d1b54e830aa8d687c296de
Alexander Grigoriev <mr.festin@yandex.ru>
2017-01-15 17:56:49 +0300

    Implement base super class

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 echo "Let's test Travis-CI!";
+
+class Super {
+    private $pictureId;
+
+    public function __construct($pictureid)
+    {
+        $this->pictureId = $pictureid;
+    }
+
+    public function getPictureId() {
+        return $this->pictureId;
+    }
+}
